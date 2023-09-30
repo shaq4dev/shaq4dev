7cfaecc94e8780eff4fa6ff5526c73561dfbe33e
Shaquille Blackwood <shaquille.blackwood95@gmail.com>
2023-09-30 19:47:26 -0400

    more changes

--- shaq4dev/src/components/Hero.jsx
+++ shaq4dev/src/components/Hero.jsx
@@ -5,13 +5,7 @@ import HeroImage from './HeroImage'
 const Hero = () => {
   return (
     <div>
-<<<<<<< HEAD
       <div className="heroContainer pt-[90px]">
-=======
-      <div className="heroContainer pt-[90px] w-[100vw] lg:flex lg:items-center">
-
-        {/* text */}
->>>>>>> c125eadaef9c60016ad521bc6b11d4f6273a97c2
         <HeroText/>
         <HeroImage/>
       </div>
